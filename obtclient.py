1ee6115212b3eef4523fbb3f0f7fa1c03db32064
h0r53 <jjm15e@my.fsu.edu>
2018-09-18 15:07:19 -0400

    Added CLI to obtclient

--- obtclient.py
+++ obtclient.py
@@ -22,23 +22,70 @@ import socket
 import smartsocket
 import pwn
 
+class OpenBinTool(object):
+    def __init__(self):
+        self.binary = None
+        self.socket = None
+        self.smartsock = None
+
+    def connect(self,host,port):
+        self.socket = socket.socket()
+        self.socket.connect((host, port))
+        self.smartsock = smartsocket.SmartSocket(self.socket)
+
+
+
+    def welcome(self):
+        print("Welcome to the OpenBinTool client!")
+        print("Enter \"h\" or \"help\" for a list of commands")
+
+    def usage(self):
+        print("Supported Commands:")
+        print("\t(l)oad FILE\t\tLoads the file named FILE")
+        print("\t(a)sm FILE\t\tAssembles instructions at FILE")
+        print("\t(d)isasm\t\tDisassembles the currently loaded file")
+        print("\t(q)uit\t\tExit program")
+        print("\t(h)elp\t\tDisplay this message")
+
 def main():
-    bin = pwn.ELF('/bin/ls')
-    s = socket.socket()
+    # Local variables
     host = 'localhost'  # needs to be in quote
     port = 11337
-    s.connect((host, port))
-    smartsock = smartsocket.SmartSocket(s)
-    smartsock.send("disasm")
-    data = smartsock.recv()
-    print(data)
-    if data == b"STATUS: OK - Begin":
-        binary = bin.get_section_by_name('.text').data()
-        smartsock.send(binary)
-        data = smartsock.recv()
-        print(data.decode('utf-8'))
+    cmd = None
+    tool = OpenBinTool()
 
-    smartsock.close()
+    # Welcome message
+    tool.welcome()
+
+    # Process user command
+    while True:
+        cmd = input("> ")
+        if cmd in ["q","quit"]:
+            break
+        elif cmd in ["h","help"]:
+            tool.usage()
+        elif cmd in ["l","load"]:
+            bin = pwn.ELF("/bin/ls") # Debug only
+            tool.binary = bin.get_section_by_name('.text').data()
+        elif cmd in ["d","disasm"]:
+            # Check to see if binary is loaded
+            if tool.binary:
+                tool.connect(host,port)
+                tool.smartsock.send("disasm")
+                data = tool.smartsock.recv()
+                print(data)
+                if data == b"STATUS: OK - Begin":
+                    # Binary
+                    tool.smartsock.send(tool.binary)
+                    data = tool.smartsock.recv()
+                    print(data.decode('utf-8'))
+                tool.smartsock.close()
+            else:
+                print("Error: no binary loaded")
+
+        else:
+            print("Command {} currently not supported".format(cmd))
+            print("Enter (h)elp for a list of commands")
 
 
 if __name__ == "__main__":
