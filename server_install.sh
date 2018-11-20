51aa8831b9441d7f9470e1116a6ad7b1817a9c9c
h0r53 <jjm15e@my.fsu.edu>
2018-11-19 20:00:32 -0500

    created installation script for server

---
+++ server_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 
+# Description: This script installs the necessary dependencies for obtserver
+
+echo "Updating system"
+apt-get update
+echo "Installing python3, python3-dev, python3-pip, and git"
+apt-get install python3 python3-dev python3-pip git
+echo "Installing pwntools for Python3"
+pip3 install --upgrade git+https://github.com/arthaud/python3-pwntools.git
+if [ ! -d ~/Repos ]; then
+	echo "Creating directory ~/Repos"
+	mkdir ~/Repos
+fi
+cd ~/Repos
+echo "Cloning radare2"
+git clone https://github.com/radare/radare2.git
+echo "Installing radare2"
+./radare2/sys/install.sh
+echo "Installing complete!"
